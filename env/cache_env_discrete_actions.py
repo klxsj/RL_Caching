ede6bbd2a544de65bc10d8af08d66664984606af
ArianNsz <a.nasehzade@gmail.com>
2020-05-21 22:51:50 -0400

    env for the result: all 12-step lifes in the edges, empty parent

--- env/cache_env_discrete_actions.py
+++ env/cache_env_discrete_actions.py
@@ -18,10 +18,10 @@ class cache_env(gym.Env):
         self.current_step = 0
         self.done = False
         self.MEM_SIZE = 6
-        self.MAX_STEPS = 500
-        self.MIN_COMMUN= 50
+        self.MAX_STEPS = 200000
+        self.MIN_COMMUN= 5
         self.MID_COMMUN= 2
-        self.MAX_COMMUN= -1
+        self.MAX_COMMUN= -3
         self.fresh_cost_weight= 1
         self.reward=0.0
         self.greedy_punishment = 0
@@ -35,12 +35,11 @@ class cache_env(gym.Env):
         Defining Observation and Action Spaces
         """
         self.action_space = spaces.Discrete(7)
-        # self.action_space = spaces.Box(low=0, high=1, shape=(3,), dtype= np.uint8)
         self.observation_space= spaces.Box(
                                            low=0,
                                            high=40,
                                            shape= (7,6),
-                                           dtype= np.uint8
+                                           dtype= np.float16
                                           )
         print(self.observation_space.sample())
         
@@ -72,11 +71,9 @@ class cache_env(gym.Env):
     
     def step(self, action):
         under_utilized = 0
-        # action = np.array(action).astype(np.uint8)
-        # action = np.unpackbits(action, bitorder='little')
-        # action = np.round(action)
         """ Calculating the reward"""
         self.reward = 0
+        self.avg_fresh=0
         if self.current_step==0:
             self.episodes=[]
         
@@ -95,11 +92,11 @@ class cache_env(gym.Env):
             p_has = 1
             
         for i in range(3):
-            a = self.current_step - self.freshness[2*i,:]
-            b = self.freshness[2*i +1,:]
-            c = np.divide(a, b, where=b!=0)
-            self.mem_status[2*i+1, :] = c
-            self.avg_fresh += sum(c)/6
+            a12 = self.current_step - self.freshness[2*i,:]
+            b12 = self.freshness[2*i +1,:]
+            c12 = np.divide(a12, b12, where=b12!=0)
+            self.mem_status[2*i+1, :] = c12
+            self.avg_fresh += sum(c12)/6
         
       #  calculate the reward for each case       
         if self.which_BS == 0:
@@ -108,14 +105,14 @@ class cache_env(gym.Env):
                 self.mem_status[0:2, :edge1_has[0][0]] = np.roll(self.mem_status[0:2, :edge1_has[0][0]], 1, axis=1)
                 self.freshness[0:2, :edge1_has[0][0]] = np.roll(self.mem_status[0:2, :edge1_has[0][0]], 1, axis=1)
                 self.reward -= self.mem_status[1, edge1_has[0][0]] #substract freshness cost
-                # if sum(action)!=0:
-                #     self.reward += self.greedy_punishment * (e1 + p_has)
+                if action!=0:
+                    self.reward += self.greedy_punishment * (e1 + p_has)
             elif p_has == 1:
                 self.mem_status[4:, :parent_has[0][0]] = np.roll(self.mem_status[2*i: 2*i+1, :parent_has[0][0]], 1, axis=1)
                 self.freshness[4:, :parent_has[0][0]] = np.roll(self.mem_status[2*i: 2*i+1, :parent_has[0][0]], 1, axis=1)
                 self.reward -= self.mem_status[5, parent_has[0][0]] #substract freshness cost
-                # if sum(action)!=0:
-                #     self.reward += self.greedy_punishment
+                if action!=0:
+                    self.reward += self.greedy_punishment
             else:
                 self.reward += self.MAX_COMMUN
                 
@@ -127,14 +124,14 @@ class cache_env(gym.Env):
                 self.mem_status[2:4, :edge2_has[0][0]] = np.roll(self.mem_status[2:4, :edge2_has[0][0]], 1, axis=1)
                 self.freshness[2:4, :edge2_has[0][0]] = np.roll(self.mem_status[2:4, :edge2_has[0][0]], 1, axis=1)
                 self.reward -= self.mem_status[3, edge2_has[0][0]] #substract freshness cost
-                if sum(action)!=0:
+                if action!=0:
                     self.reward += self.greedy_punishment * (e2 + p_has)
             elif p_has == 1:
                 self.mem_status[4:, :parent_has[0][0]] = np.roll(self.mem_status[2*i: 2*i+1, :parent_has[0][0]], 1, axis=1)
                 self.freshness[4:, :parent_has[0][0]] = np.roll(self.mem_status[2*i: 2*i+1, :parent_has[0][0]], 1, axis=1)
                 self.reward -= self.mem_status[5, parent_has[0][0]] #substract freshness cost
-                # if sum(action)!=0:
-                #     self.reward += self.greedy_punishment
+                if action!=0:
+                    self.reward += self.greedy_punishment
             else:
                 self.reward += self.MAX_COMMUN
     
@@ -147,38 +144,46 @@ class cache_env(gym.Env):
         self.mem_status[expired[0]*2 + 1, expired[1]] = 0
         self.freshness[expired[0]*2, expired[1]] = 0 
         self.freshness[expired[0]*2 + 1, expired[1]] = 0
+
         
-        while self.current_step > 20:
-            under_utilized = np.where(self.mem_status[self.mem_slots, :] == 0)
-            under_utilized = max(under_utilized[0].shape[0], under_utilized[1].shape[0])
-            self.reward -= under_utilized
         
         
         """ Calling the required functions and returning the obs & reward"""
         self._take_action(action)
+        
+        under_utilized = np.where(self.mem_status[self.mem_slots, :] == 0)
+        under_utilized = max(under_utilized[0].shape[0], under_utilized[1].shape[0])#/9
+        under_utilized = np.clip(under_utilized, 0, 2)
+        self.reward -= under_utilized
+        # self.reward = np.clip(self.reward, -21, 100)
+        
+        
         obs= self._next_observation()
         self.current_step += 1
-        self.done = self.current_step > self.MAX_STEPS
+        self.done = self.current_step >= self.MAX_STEPS
         self.episodes.append(self.reward)
-        # assert obs.shape==(7,6)
-        # assert type(self.reward)==float
         return obs, self.reward, self.done, {}
 
 
     def reset(self):
-        self.current_step = 0
+        # print('    !!!!!!    RESTARTED    !!!!')
+        # print('    !!!!!!    RESTARTED    !!!!')
+        self.current_step = np.random.randint(0, 10000)
         self.done = False
         self.reward= 0
+        self.avg_fresh=0
         self.mem_status= np.zeros(shape=(6,6))
         self.freshness= np.zeros(shape=(6,6))
+        self.freshness[self.mem_slots, :] = self.current_step
         self.line1= np.zeros(shape=(1,6))
+        self.episodes=[]
         return self._next_observation()
 
 
     def _take_action(self, action):
         # action = np.round(action)
         action_final = np.array(action).astype(np.uint8)
-        action_final = np.unpackbits(action, bitorder='little')
+        action_final = np.unpackbits(action_final, bitorder='little')
         self.action = action_final
         request = self.df.loc[self.current_step].values
         for i in range(3):
@@ -189,8 +194,8 @@ class cache_env(gym.Env):
                     self.mem_status[2*i+1, empty[0][-1]] = 0
                     self.freshness[2*i, empty[0][-1]] = self.current_step
                     self.freshness[2*i+1, empty[0][-1]] = request[1]
-                    self.mem_status[2*i: 2*i+2, :empty[0][-1]+1] = np.roll(self.mem_status[2*i: 2*i+1, :empty[0][-1]+1], 1, axis=1)
-                    self.freshness[2*i: 2*i+2, :empty[0][-1]+1] = np.roll(self.freshness[2*i: 2*i+1, :empty[0][-1]+1], 1, axis=1)
+                    self.mem_status[2*i: 2*i+2, :empty[0][-1]+1] = np.roll(self.mem_status[2*i: 2*i+2, :empty[0][-1]+1], 1, axis=1)
+                    self.freshness[2*i: 2*i+2, :empty[0][-1]+1] = np.roll(self.freshness[2*i: 2*i+2, :empty[0][-1]+1], 1, axis=1)
                 else:
                     self.mem_status[2*i, -1] = request[0]
                     self.mem_status[2*i+1, -1] = 0
@@ -210,10 +215,15 @@ class cache_env(gym.Env):
         """   """
         print(f'Step: {self.current_step}')
         print(f'action: {self.action}')
-        print(f'Next Requests: {self.df.loc[self.current_step: self.current_step+1].values}')
-        print(f'Memory Status: {self.mem_status}')
+        print(f'Next Requests: {self.df.loc[self.current_step].values}')
+        ops = np.concatenate((self.line1, self.mem_status), axis=0)
+        ops = np.around(ops, decimals=3)
+        print(f'Observation: {ops}')
+        fresh2 = np.around(self.freshness, decimals=3)
+        print(f'Freshnes: {fresh2}')
         print(f'Reward: {self.reward}')
-        if self.current_step== 499 :
+        if self.current_step== 500 :
             plt.plot(self.episodes)
-        print(len(self.episodes))
+            print(f'first reward: {self.episodes[0]}')
+            print(f'lastreward: {self.episodes[-1]}')
             
